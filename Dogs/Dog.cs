5af40f42fbea7da7b3d914d44af876be51439b6b
Lavolin <lavolin@gmail.com>
2021-12-15 15:26:06 +0200

    Add project files.

---
+++ Dogs/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dogs
+{
+    class Dog
+    {
+        public Dog()            // default constructor
+        {
+            Name = "Bau";
+        }
+        public Dog(string name, string breed, int age)      // constructor!
+        {
+            Name = name;
+            Breed = breed;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public string Breed { get; set; }
+
+        public int Age { get; set; }
+
+        public void SayHi()
+        {
+            Console.WriteLine("Hey from dog!");
+            Console.WriteLine($"My name is {Name} Breed: {Breed}");
+            Console.WriteLine("Bye");
+        }
+    }
+}
---
+++ Dogs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Dogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog("Sharo", "Pitbull", 3); // with constructor
+            //{
+            //    Name = "Sharo",
+            //    Age = 2,
+            //    Breed = "Pitbull"
+            //};
+            Dog dog2 = new Dog("Ronny", "Street", 2);
+            //{
+            //    Name = "Ronny",
+            //    Age = 2,
+            //    Breed = "Street"
+            //};
+
+            //dog.Name = "Sharo";
+            //dog.Age = 2;
+            Dog dog3 = new Dog();
+            //Console.WriteLine(dog.Name);
+            //Console.WriteLine(dog.Breed);
+            dog1.SayHi();
+            dog2.SayHi();
+            dog3.SayHi();
+        }
+    }
+}
