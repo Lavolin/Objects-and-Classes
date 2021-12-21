f78b2280edde56003dd607aebcdfd4fecd2863f3
Lavolin <lavolin@gmail.com>
2021-12-21 22:26:40 +0200

    21.12

---
+++ 04 Students/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_Students
+{
+    class Student
+    {
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:f2}";
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split(" ");
+                Student student = new Student();
+                student.FirstName = tokens[0];
+                student.LastName = tokens[1];
+                student.Grade = double.Parse(tokens[2]);
+                students.Add(student);
+
+            }
+            students.OrderByDescending(student => student.Grade).ToList().ForEach(Console.WriteLine);
+            //students = students.OrderByDescending(student => student.Grade).ToList();
+            //foreach (Student student in students)
+            //{
+            //    Console.WriteLine(student);
+            //}
+        }
+    }
+}
---
+++ 06 Vehicle Catalogue/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _06_Vehicle_Catalogue
+{
+    internal class Program
+    {
+        internal enum VehicleType
+        {
+            Car,
+            Truck
+        }
+        internal class Vehicle
+        {
+            public Vehicle(VehicleType type, string model, string color, int horsepower)
+            {
+                Type = type;
+                Model = model;                  // constructor
+                Color = color;
+                Horsepower = horsepower;
+            }
+            public VehicleType Type { get; } // private fields
+            public string Model { get; }
+            public string Color { get; }
+            public int Horsepower { get; }
+
+            public override string ToString()
+            {
+                var builder = new StringBuilder();
+                builder.AppendLine($"Type: {Type}");
+                builder.AppendLine($"Model: {Model}");
+                builder.AppendLine($"Color: {Color}");
+                builder.AppendLine($"Horsepower: {Horsepower}");
+                return builder.ToString().TrimEnd();
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                if (cmd == "End") break;
+                
+                var tokens = cmd.Split();
+                VehicleType vehicleType;
+                bool isVehicleTypeParsable = Enum.TryParse(tokens[0], true, out vehicleType);
+                if (isVehicleTypeParsable)
+                {
+                    string currModel = tokens[1];
+                    string currColor = tokens[2];
+                    int currHorsepower = int.Parse(tokens[3]);
+                    var currVehicle = new Vehicle(vehicleType, currModel, currColor, currHorsepower);
+                    vehicles.Add(currVehicle);
+                }
+
+            }
+
+            while (true)
+            {
+                string cmdArgs = Console.ReadLine();
+                if (cmdArgs == "Close the Catalogue") break;
+
+                Vehicle desiredVehicle = vehicles.FirstOrDefault(vehicle => vehicle.Model == cmdArgs);
+                Console.WriteLine(desiredVehicle);
+            }
+            var cars = vehicles.Where(car => car.Type == VehicleType.Car);
+            var trucks = vehicles.Where(truck => truck.Type == VehicleType.Truck);
+            double carsAvrHp = cars.Any() ? cars.Average(car => car.Horsepower) : 0.0;
+            double trucksAvrHp = trucks.Any() ? trucks.Average(truck => truck.Horsepower) : 0.0;
+            Console.WriteLine($"Cars have average horsepower of: {carsAvrHp:f2}.");
+            Console.WriteLine($"Trucks have average horsepower of: {trucksAvrHp:f2}.");
+        }
+    }
+}
